653db289d46a321fdbb3f4394ed131b62820e5c7
Jessica(Jessie) Politron <41178567+jpolitron@users.noreply.github.com>
2021-10-15 19:49:54 -0700

    Add files via upload

---
+++ Adventure_game.py
@@ -0,0 +1,21 @@
+import time
+playing = True
+print("You are studying for big math test in the library.\n")
+time.sleep(2)
+print("You leave the room to go to the restroom you endup in a country sidehouse kitchen it is dark do you\n")
+time.sleep(2)
+print("Enter 1 to go back to the door you came from.\n")
+time.sleep(2)
+print("Enter 2 to try to leave through the front door.\n")
+time.sleep(2)
+print("What would you like to do?\n")
+choice= input("Please enter 1 or 2\n")
+
+while playing == True:
+		print("You end up back to the library it's night and everyones gone")
+		playing = False
+	else:
+		print("You open the front door and in front of you is a corn field across the street and you notcie A BLACK FIGURE STANDING THERE AND THEN IT STARTS RUNNING TOWARDS YOU!!!") 
+		playing = False
+
+
